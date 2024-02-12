1a36158fb7e039d98f79bf4e376c24461b4e31bd
ruimp <rmp@ruimp.com>
2024-02-12 23:15:55 +0000

    Swap leap for flash

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,36 +3,43 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    local trouble = require("trouble")
-    -- remove conflict with leap.nvim
-    trouble.setup({
-      actions_keys = {
-        switch_severity = "<C-s>"
-      },
-    })
-    -- keymaps
-    vim.keymap.set("n", "<leader>xx", trouble.toggle,
-      { desc = "Toggle" })
-    vim.keymap.set("n", "<leader>xw", function()
-        trouble.toggle("workspace_diagnostics")
-      end,
-      { desc = "Workspace" })
-    vim.keymap.set("n", "<leader>xd", function()
-        trouble.toggle("document_diagnostics")
-      end,
-      { desc = "Document" })
-    vim.keymap.set("n", "<leader>xq", function()
-        trouble.toggle("quickfix")
-      end,
-      { desc = "Quickfix" })
-    vim.keymap.set("n", "<leader>xl", function()
-        trouble.toggle("loclist")
-      end,
-      { desc = "Loclist" })
-    vim.keymap.set("n", "<leader>gR", function()
-        trouble.toggle("lsp_references")
-      end,
-      { desc = "References" })
-  end
+  opts = {},
+  keys = {
+    {
+      "<leader>xx",
+      mode = "n",
+      function() require("trouble").toggle() end,
+      desc = "Toggle",
+    },
+    {
+      "<leader>xw",
+      mode = "n",
+      function() require("trouble").toggle("workspace_diagnostics") end,
+      desc = "Workspace",
+    },
+    {
+      "<leader>xd",
+      mode = "n",
+      function() require("trouble").toggle("document_diagnostics") end,
+      desc = "Document",
+    },
+    {
+      "<leader>xq",
+      mode = "n",
+      function() require("trouble").toggle("quickfix") end,
+      desc = "Quickfix",
+    },
+    {
+      "<leader>xl",
+      mode = "n",
+      function() require("trouble").toggle("loclist") end,
+      desc = "Loclist",
+    },
+    {
+      "<leader>gR",
+      mode = "n",
+      function() require("trouble").toggle("lsp_references") end,
+      desc = "References",
+    }
+  },
 }
