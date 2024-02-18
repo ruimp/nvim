2271f08d95ad07ca1edd44c62c7adb27903c384c
ruimp <rmp@ruimp.com>
2024-02-18 20:42:14 +0000

    add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	keys = {
+		{
+			"<leader>fo",
+			mode = "n",
+			"<cmd>Oil<cr>",
+			desc = "Oil",
+		},
+	},
+}
