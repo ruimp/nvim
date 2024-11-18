7bb29c2df4985116fd20ebf7bfb1ffff01e600b6
Rui Peixoto <rmp@rmpMac.local>
2024-11-17 20:17:27 -0500

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-  "mellow-theme/mellow.nvim",
+  "rebelot/kanagawa.nvim",
   priotity = 1000,
   lazy = false,
   config = function()
-    vim.g.mellow_italic_functions = true
-    vim.g.mellow_bold_functions = true
-    vim.cmd.colorscheme("mellow")
+    vim.cmd.colorscheme("kanagawa")
   end,
 }
